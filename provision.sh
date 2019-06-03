780964943e5164ecb28806f68f63bad694e08ca3
privatereese <5148715+privatereese@users.noreply.github.com>
2019-06-03 17:06:03 +0200

    added vagrantfiles to LSTM repository

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#######################
+#
+# This is a provision script
+# it will be called once when the vagrant vm is first provisioned
+# If you have commands that you want to run always please have a
+# look at the bootstrap.sh script
+#
+# Contributor: Bernhard Blieninger, Robert Hamsch
+######################
+
+sudo apt update -qq
+sudo apt install python3.5 python3-pip tmux -qq
+
